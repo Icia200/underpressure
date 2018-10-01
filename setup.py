176ef0fbb177c2432759de962bcece36fae9975a
Icia200 <31482450+Icia200@users.noreply.github.com>
2018-10-01 21:34:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='underpressure',
+      version='0.1',
+      description='Counts. Down.',
+      url='https://github.com/Icia200/underpressure',
+      author='Isaac Renton & Simon Toogood',
+      license='MIT',
+      packages=['underpressure'],
+      zip_safe=False)
