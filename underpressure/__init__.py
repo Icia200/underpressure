a333a02fa18c64d392864c9268872ab7621808f2
Icia200 <31482450+Icia200@users.noreply.github.com>
2018-10-01 22:57:14 +0100

    Setup for live update and testing

--- underpressure/__init__.py
+++ underpressure/__init__.py
@@ -56,7 +56,7 @@ def update():
         root.after(200, update)
 
 
-title = Label(font=("ansifixed", 50), bg="black", fg="white", text="Test Update String I:")
+title = Label(font=("ansifixed", 50), bg="black", fg="white", text="ICT Deadline (updateMe):")
 timel = Label(font=("ansifixed", 90), bg="black", fg="white", text="")
 
 title.pack(expand=True)
